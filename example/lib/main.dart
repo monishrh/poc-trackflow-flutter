47b2cfde9f3090b6a02a00880038c84d35823223
monishsynclovis <monish.rh@synclovis.com>
2020-08-11 20:53:00 +0530

    Tracking API CALL

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -7,11 +5,7 @@ import 'package:flutter_trackflow/flutter_trackflow.dart';
 
 void main() => runApp(HandlePermissionDemo());
 
-
 HandlePermissionDemoState pageState;
-
-
-
 
 class HandlePermissionDemo extends StatefulWidget {
   @override
@@ -20,42 +14,50 @@ class HandlePermissionDemo extends StatefulWidget {
     return pageState;
   }
 }
-class HandlePermissionDemoState extends State<HandlePermissionDemo>{
- 
-  
 
- @override
+class HandlePermissionDemoState extends State<HandlePermissionDemo> {
+  @override
   void initState() {
     super.initState();
     initList();
   }
 
-
   void initList() async {
-    var list=await  PermissionHandler().permissionList();
-    list.forEach((item){print(item.group.toString() + item.status.toString());});
- 
+    // var list=await  PermissionHandler().permissionList();
+    // list.forEach((item){print(item.group.toString() + item.status.toString());});
+    var baseUrl = "https://jsonplaceholder.typicode.com";
+    var method = "POST";
+    var postdata = {"id": 1, "name": 'Moni'};
+    int connectTimeout = 5000;
+    int receiveTimeout = 3000;
+    var list = await PermissionHandler().apiCall(
+        "/posts", method, baseUrl, connectTimeout, receiveTimeout, postdata);
+    //For URL print(list.request.path);
+    // For Response Data print(list.data);
+    // For Request body print(list.request.data);
+    // For Method print(list.request.data);
+    // For Response Headers print(list.headers);
+    // For Status Code print(list.statusCode);
+    // For  Request Headers print(list.request.headers;
+
+    print(list.request.data);
   }
 
- 
- 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-              title: Text("Permission List"),
-               actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {
-              initList();
-            },
-          ),
-        
-        ],
-          ),
+        appBar: AppBar(
+          title: Text("Permission List"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                initList();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
--- lib/src/permission_handler.dart
+++ lib/src/permission_handler.dart
@@ -6,12 +6,19 @@ import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 import 'permission_enums.dart';
 import 'codec.dart';
+import 'package:dio/dio.dart';
 
 class Item {
   PermissionGroup group;
   PermissionStatus status;
 
   Item(this.group, this.status);
+}
+
+class NetworkItem {
+  String method;
+  String url;
+  NetworkItem(this.method, this.url);
 }
 
 /// Provides a cross-platform (iOS, Android) API to request and check permissions.
@@ -43,19 +50,48 @@ class PermissionHandler {
     return Codec.decodePermissionStatus(status);
   }
 
-  Future <List<Item>> permissionList () async {
+  Future<List<Item>> permissionList() async {
     List<Item> list = [];
 
     for (var i = 0; i < PermissionGroup.values.length; i++) {
-    PermissionStatus status = await PermissionHandler().checkPermissionStatus(PermissionGroup.values[i]);
-     
-        if (status == PermissionStatus.granted || status == PermissionStatus.denied) {
-          list.add(Item(PermissionGroup.values[i], status));
-       } 
+      PermissionStatus status = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.values[i]);
+
+      if (status == PermissionStatus.granted ||
+          status == PermissionStatus.denied) {
+        list.add(Item(PermissionGroup.values[i], status));
+      }
     }
 
     return Future.value(list);
-    
+  }
+
+  Future apiCall(url, method, baseUrl, connectTimeout, receiveTimeout,data) async {
+    Response response;
+
+    BaseOptions options = new BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: connectTimeout,
+      receiveTimeout: receiveTimeout,
+    ); // with default Options
+
+    Dio dio = new Dio(options);
+    if (method == "GET") {
+      try {
+        response = await dio.get(url);
+        return response;
+      } catch (e) {
+        return e;
+      }
+    }
+     if (method == "POST") {
+      try {
+        response = await dio.post(url,data: data);
+        return response;
+      } catch (e) {
+        return e;
+      }
+    }
   }
 
   /// Request to see if you should show a rationale for requesting permission.
